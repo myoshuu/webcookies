84708ef16e91229e6413fe728211c0230d54bf41
myoshuu <myoshu.me@gmail.com>
2024-05-18 21:44:59 +0700

    fix scroll bug lazy loading

--- js/lazyLoading.js
+++ js/lazyLoading.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const loading = document.getElementById("loading");
-  const content = document.getElementById("content");
 
   window.addEventListener("load", () => {
     loading.style.display = "none";
-    content.style.display = "block";
     document.body.style.overflow = "auto";
   });
 
